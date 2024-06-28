ed69cdf19ab52f8ea7ee4f5ff0efb6213cb659c7
Juan David Ospina <juan.ospinal@outlook.com>
2024-06-28 13:39:43 -0500

    feat(student): add delete operation

---
+++ app/student/router.py
@@ -0,0 +1,85 @@
+from fastapi import APIRouter, HTTPException
+from sqlmodel import select, func
+
+from app.common.deps import DBDepends
+from .schemas import StudentList, StudentPublic, StudentBase, StudentUpdate
+from .models import Student
+
+
+router = APIRouter(prefix="/api/students", tags=["Students"])
+
+
+@router.get("/{id}", response_model=StudentPublic, status_code=200)
+def get_student_by_id(id: int, db: DBDepends) -> StudentPublic:
+    statement = select(Student).where(Student.id == id)
+    student = db.exec(statement).first()
+
+    if not student:
+        raise HTTPException(
+            status_code=404, 
+            detail="The student with this id does not exist in the system"
+        )
+    
+    return student
+
+
+@router.get("/", response_model=StudentList, status_code=200)
+def get_students(db: DBDepends):
+    """
+    Retrieve students.
+    """
+    count_statement = select(func.count()).select_from(Student)
+    count = db.exec(count_statement).one()
+
+    statement = select(Student)
+    students = db.exec(statement).all()
+
+    return StudentList(data=students, count=count)
+
+
+@router.post("/", response_model=StudentPublic, status_code=201)
+def create_student(db: DBDepends, data: StudentBase):
+    """
+    Create a new student.
+    """
+    try:
+        student = Student.model_validate(data, strict=True)
+        db.add(student)
+        db.commit()
+        db.refresh(student)
+        return student
+    except Exception:
+        db.rollback()
+        raise HTTPException(status_code=500, detail="Server error")
+    
+
+@router.put("/{id}", status_code=204,
+            responses={204: {"description": "Successful update"}})
+def update_student(id: int, db: DBDepends, data: StudentUpdate) -> None:
+    """
+    Update a student.
+    """
+    statement = select(Student).where(Student.id == id)
+    student = db.exec(statement).first()
+
+    if student is None:
+        raise HTTPException(
+            status_code=404, 
+            detail="The student with this id does not exist in the system"
+        )
+    
+    student.sqlmodel_update(data.model_dump())
+    db.add(student)
+    db.commit()
+    db.refresh(student)
+
+
+@router.delete("/{id}", status_code=204, 
+               responses={204: {"description": "Successful delete"}})
+def delete_student(id: int, db: DBDepends):
+    """
+    Delete a student.
+    """
+    student = get_student_by_id(id, db)
+    db.delete(student)
+    db.commit()
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
-from sqlmodel import select, func
 
-from app.student.models import Student
-from app.student.schemas import StudentList, StudentBase, StudentPublic, StudentUpdate
-from app.common.deps import DBDepends
+from app.student.router import router as student_router
 
 
 app = FastAPI(
@@ -17,48 +14,5 @@ def index():
     return RedirectResponse("/docs")
 
 
-@app.get("/students", tags=["Students"], response_model=StudentList)
-def get_students(db: DBDepends):
-    """
-    Retrieve students.
-    """
-    count_statement = select(func.count()).select_from(Student)
-    count = db.exec(count_statement).one()
-
-    statement = select(Student)
-    students = db.exec(statement).all()
-
-    return StudentList(data=students, count=count)
-
-
-@app.post("/students", tags=["Students"], response_model=StudentPublic)
-def create_student(db: DBDepends, data: StudentBase):
-    """
-    Create a new student.
-    """
-    try:
-        student = Student.model_validate(data, strict=True)
-        db.add(student)
-        db.commit()
-        db.refresh(student)
-        return student
-    except Exception:
-        db.rollback()
-        raise HTTPException(status_code=500, detail="Server error")
-    
-
-@app.put("/students/{id}", tags=["Students"], responses={204: {"description": "Update succesfully"}})
-def update_student(id: int, db: DBDepends, data: StudentUpdate) -> None:
-    statement = select(Student).where(Student.id == id)
-    student = db.exec(statement).first()
-
-    if student is None:
-        raise HTTPException(
-            status_code=404, 
-            detail="The user with this id does not exist in the system"
-        )
-    
-    student.sqlmodel_update(data.model_dump())
-    db.add(student)
-    db.commit()
-    db.refresh(student)
+# Api routes
+app.include_router(student_router)
